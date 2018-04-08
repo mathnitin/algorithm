242e7b0d27b1858c69473e1b858ebc12b3668a14
nitinmat <nitinmat@cisco.com>
2018-04-08 12:38:42 -0700

    Complete code to find maximum height of tree

--- Tree/HeightTree.py
+++ Tree/HeightTree.py
@@ -2,33 +2,67 @@
 
 '''
 Find the maximum height of the tree
+
+Input:
+      1
+    /  \
+   2    3
+ /
+4
+
+Output: 3
+
+Input:
+                  1
+               /      \
+              2        3
+           /   \      /  \ 
+          4     5    6    7
+                    /
+                    8
+Output: 4
 '''
 
-class Node(self):
+class Node():
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-class Tree(self):
-    def __init__(self):
-        self.root = None
+class Tree():
+    def __init__(self, node):
+        self.root = node
 
-    def addNode(self, data, rootNode):
-        if rootNode == None:
-            node = Node(data)
-            self.root = node
-        if data > rootNode.data:
-            if rootNode.right != None:
-                self.addNode(data, rootNode.right)
-            else:
-                node = Node(data)
-                rootNode.right = node
-        if data <= rootNode.data:
-            if rootNode.left != None:
-                self.addNode(data, rootNode.left)
-            else:
-                node = Node(data)
-                rootNode.left = node
-            
-    
+def __findHeight(node):
+    if node is None:
+        return 0
+    leftHeight = __findHeight(node.left)
+    rightHeight = __findHeight(node.right)
+    return max(leftHeight,rightHeight)+1
+
+def findHeight(tree):
+    rootNode = tree.root
+    if rootNode is None:
+        return 0
+    return __findHeight(rootNode)
+
+
+if __name__ == "__main__":
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(5)
+    n6 = Node(6)
+    n7 = Node(7)
+    n8 = Node(8)
+    tree = Tree(n1)
+    n1.left = n2
+    n1.right = n3
+    n2.left = n4
+    print "Tree Height: ", findHeight(tree)
+    n2.right = n5
+    n3.left = n6
+    n3.right = n7
+    n6.left = n8
+    print "Tree Height: ", findHeight(tree)
